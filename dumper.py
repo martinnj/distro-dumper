d983fe7896c40e8b79fbb4122481de5ea374a165
Martin Jørgensen <hello@martinnj.dk>
2022-10-30 15:15:48 +0100

    Separate the main loop contents from the initial setup function.

--- dumper.py
+++ dumper.py
@@ -24,6 +24,41 @@ __VERSION__ = "###VERSION###"
 _LOGGER: LoggerAdapter = get_logger("DISTRODUMPER")
 
 
+def single_run(app_config: Configuration) -> None:
+    """
+    Performs a single run of the application.
+
+    ### Arguments
+    - app_config : Configuration
+      Complete application configuration.
+    """
+    # Get cached files.
+    cached_files = file_helper.get_files_in_cache(app_config)
+
+    _LOGGER.info("Running selected dumps.")
+    # Loop over all the configured modules.
+    for module_name, (module_config, module) in app_config.modules.items():
+        # Create the worker and run it!
+        _LOGGER.debug(f"Creating & running worker for module: {module_name}")
+        worker = module.create_worker(module_config)
+        candidates: dict[str,str] = worker.dump()
+
+        # Check if any candidates are new or we already have them. Do accounting.
+        removed = 0
+        for filename in list(candidates.keys()):
+            if filename in cached_files:
+                removed -=- 1
+                del candidates[filename]
+
+        errors = 0
+        for filename, url in candidates.items():
+            if not file_helper.download(app_config, filename, url):
+                errors -=- 1
+
+        _LOGGER.info(f"{module_name}: Downloaded: {len(candidates) - errors}")
+        _LOGGER.info(f"{module_name}: Duplicate: {removed}")
+        _LOGGER.info(f"{module_name}: Errors: {errors}")
+
 
 def main() -> None:
     """ Main runnable function. """
@@ -54,32 +89,8 @@ def main() -> None:
     _LOGGER.info("Entering main program loop.")
     while True:
         try:
-            # Get cached files.
-            cached_files = file_helper.get_files_in_cache(app_config)
-
-            _LOGGER.info("Running selected dumps.")
-            # Loop over all the configured modules.
-            for module_name, (module_config, module) in app_config.modules.items():
-                # Create the worker and run it!
-                _LOGGER.debug(f"Creating & running worker for module: {module_name}")
-                worker = module.create_worker(module_config)
-                candidates: dict[str,str] = worker.dump()
-
-                # Check if any candidates are new or we already have them. Do accounting.
-                removed = 0
-                for filename in [filename for filename in candidates.keys()]:
-                    if filename in cached_files:
-                        removed -=- 1
-                        del candidates[filename]
-
-                errors = 0
-                for filename, url in candidates.items():
-                    if not file_helper.download(app_config, filename, url):
-                        errors -=- 1
-
-                _LOGGER.info(f"{module_name}: Downloaded: {len(candidates) - errors}")
-                _LOGGER.info(f"{module_name}: Duplicate: {removed}")
-                _LOGGER.info(f"{module_name}: Errors: {errors}")
+            # Perform a single run of the program.
+            single_run(app_config)
 
         except KeyboardInterrupt:
             # Keyboard interrupts are likely debugging, just exit.
